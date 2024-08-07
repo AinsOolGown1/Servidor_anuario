1c0f41029596d78bccddafac6e5b18673bfdd40e
AinsOolGown1 <moralesisaac900@gmail.com>
2024-08-07 11:01:36 -0600

    Cambios en la variable de .env

--- config/db.js
+++ config/db.js
@@ -1,15 +1,16 @@
-const mongoose = require( 'mongoose' );
-const dotenv = require('dotenv').config();
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const conectarDB = async () => {
   try {
-    await mongoose.connect(process.env.DB_MONGO, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    const uri = process.env.DB_MONGO;  // Asegurarse que la variable de entorno esté definida
+    if (!uri) {
+      throw new Error('La URI de la base de datos no está definida en las variables de entorno.');
+    }
+    await mongoose.connect(uri);  // Sin las opciones obsoletas
     console.log('Conexión a la BD exitosa');
   } catch (error) {
-    console.log(error);
+    console.error('Error al conectar a la base de datos:', error);
     process.exit(1); // Detenemos la aplicación
   }
 };
